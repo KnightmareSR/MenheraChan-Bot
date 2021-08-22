d3487da4213808c4e7d38547efa69305a14c8854
KnightmareSR <79394927+KnightmareSR@users.noreply.github.com>
2021-08-22 02:49:12 -0400

    Inital Commit
    poggers

---
+++ The New Bot/Commands.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+
+namespace DiscordBot.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+
+
+
+
+
+
+
+
+
+
+        //var _invite = await _server.CreateInvite(maxAge: null, maxUses: 25, tempMembership: false, withXkcd: false);
+
+
+        [Command("Kick")]
+        [RequireUserPermission(GuildPermission.KickMembers, ErrorMessage = "Invalid Permission to kick this user!")]
+
+        public async Task Kick(IGuildUser user, [Remainder] string reason = null)
+        {
+
+
+
+            if (user.GuildPermissions.KickMembers)
+            {
+                await user.KickAsync(reason);
+
+            }
+
+            else
+
+            {
+                await user.KickAsync();
+                
+            }
+            
+
+            var EmbedBuilder = new EmbedBuilder()
+
+              .WithDescription($":white_check_mark: {user.Mention} Was Kicked!\n**Reason** {reason}")
+            .WithFooter(footer =>
+
+            {
+
+
+                footer
+                .WithText("User Kick Log");
+
+
+
+            });
+
+            Embed embed = EmbedBuilder.Build();
+            await ReplyAsync(embed: embed);
+
+
+            ITextChannel logChannel = Context.Client.GetChannel(838954527292915773) as ITextChannel;
+
+            var EmbedBuilderLog = new EmbedBuilder()
+
+
+         .WithDescription($"{user.Mention} Was Banned!\n**Reason** {reason}\n**Moderator** {Context.User.Mention}")
+         .WithFooter(footer =>
+
+
+
+         {
+
+
+             footer
+             .WithText("User Kick Log");
+
+
+
+         });
+
+            Embed embedLog = EmbedBuilderLog.Build();
+            await
+            logChannel.SendMessageAsync(embed: embedLog);
+
+
+
+        }
+
+
+
+
+        [Command("Ban")]
+
+        [RequireUserPermission(GuildPermission.BanMembers, ErrorMessage = "Invalid Permission to ban this user!")]
+        public async Task BanMemeber(IGuildUser user = null, [Remainder] string reason = null)
+        {
+            if (user == null)
+            {
+                await ReplyAsync("Please Specify a user!");
+                return;
+
+            }
+            if (reason == null) reason = "Not Specified";
+
+
+            await Context.Guild.AddBanAsync(user, 1, reason);
+
+            var EmbedBuilder = new EmbedBuilder()
+
+
+            .WithDescription($":white_check_mark: {user.Mention} Was Banned!\n**Reason** {reason}")
+            .WithFooter(footer =>
+
+
+
+            {
+
+
+                footer
+                .WithText("User Ban Log");
+
+
+
+            });
+
+            Embed embed = EmbedBuilder.Build();
+            await ReplyAsync(embed: embed);
+
+
+            ITextChannel logChannel = Context.Client.GetChannel(838954527292915773) as ITextChannel;
+
+            var EmbedBuilderLog = new EmbedBuilder()
+
+
+         .WithDescription($"{user.Mention} Was Banned!\n**Reason** {reason}\n**Moderator** {Context.User.Mention}")
+         .WithFooter(footer =>
+
+
+
+         {
+
+
+             footer
+             .WithText("User Ban Log");
+
+
+
+         });
+
+            Embed embedLog = EmbedBuilderLog.Build();
+            await
+            logChannel.SendMessageAsync(embed: embedLog);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ The New Bot/Program.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+
+namespace DiscordBot
+{
+    public class Program
+    {
+        static void Main(String[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+
+
+
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+
+
+        public async Task RunBotAsync()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+            _services = new ServiceCollection()
+            .AddSingleton(_client)
+            .AddSingleton(_commands)
+            .BuildServiceProvider();
+
+            //Channel Token
+
+            string token = "ODM4NTU3MTIxMzk3MTI5Mjg2.YI81LQ.pffaVlKiPqgoI1aB72Xs6DEH1yk";
+
+
+            _client.Log += _client_Log;
+
+            await RegisterCommandsAsync();
+
+            await _client.LoginAsync(TokenType.Bot, token);
+
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+
+
+        }
+
+
+        private Task _client_Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+
+
+        public async Task RegisterCommandsAsync()
+        {
+           _client.MessageReceived += HandleCommandAsync;
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage messageParam)
+        {
+            // Don't process the command if it was a system message
+            var message = messageParam as SocketUserMessage;
+            if (message == null) return;
+
+            // Create a number to track where the prefix ends and the command begins
+            int argPos = 0;
+
+            // Determine if the message is a command based on the prefix and make sure no bots trigger commands
+            if (!(message.HasCharPrefix('.', ref argPos) ||
+                message.HasMentionPrefix(_client.CurrentUser, ref argPos)) ||
+                message.Author.IsBot)
+                return;
+
+            // Create a WebSocket-based command context based on the message
+            var context = new SocketCommandContext(_client, message);
+
+            // Execute the command with the command context we just
+            // created, along with the service provider for precondition checks.
+            await _commands.ExecuteAsync(
+                context: context,
+                argPos: argPos,
+                services: null);
+        }
+
+
+
+
+    }
+}
